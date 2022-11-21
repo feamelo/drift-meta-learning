e19bc8042f4f87ccc50c1558331dfeedc184905b
Fernanda Amaral <fernanda.amaral.melo@gmail.com>
2022-11-21 09:17:35 -0300

    final experiment run

--- src/experiments/run_drift_comp.py
+++ src/experiments/run_drift_comp.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from utils import DriftContributionGenerator
 from utils import DATASETS_METADATA, MODELS_METADATA
 
@@ -12,15 +13,16 @@ def get_window_size(metadata: dict) -> int:
 
 
 # Run different datasets and base models
+start = time.time()
 for base_model in MODELS_METADATA.keys():
     for dataset_name, dataset_metadata in DATASETS_METADATA.items():
-        # for n_features in range(10, 101, 5):
-        n_features = 5
-        d_gen = DriftContributionGenerator(
-            base_model=base_model,
-            dataset_name=dataset_name,
-            train_batch_size=get_window_size(dataset_metadata),
-            meta_model_params={"select_k_features": n_features/100},
-            output_filename=f"base_model: {base_model} - dataset: {dataset_name} - select_k_features: {n_features}%"
-        )
-        d_gen.run()
+        for n_features in range(5, 101, 5):
+            d_gen = DriftContributionGenerator(
+                base_model=base_model,
+                dataset_name=dataset_name,
+                train_batch_size=get_window_size(dataset_metadata),
+                meta_model_params={"select_k_features": n_features/100},
+                output_filename=f"base_model: {base_model} - dataset: {dataset_name} - select_k_features: {n_features}%"
+            )
+            d_gen.run()
+print(f"Finished - elapsed time: {time.time() - start}")
